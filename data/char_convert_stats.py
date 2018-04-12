550757f9597a5587d8c591f024be4fe2ce963676
qianchu <hey_flora@126.com>
2018-04-12 23:54:28 +0100

    text extraction, sent segmentation, trad2simp dict
    Extract ldc data and sentence segmentation 
    
    In the directory of ldc file, run 
    
    python process_ldc_file.py ./ ./trad_lines 
    
    Output to trad_lines 
    
    Run opencc 
    
    Git clone https://github.com/BYVoid/OpenCC.git 
    
    opencc -i trad_lines -o ./simp_lines -c ./OpenCC/data/config/tw2s.json 
    
    Check if two lines have different number of chars and output tra2simp and simp2tra dictionaries 
    
    python char_convert_stats.py ./trad_lines ./simp_lines

---
+++ data/char_convert_stats.py
@@ -0,0 +1,46 @@
+
+
+import sys
+import pickle
+from collections import defaultdict
+import json
+
+
+
+trad_f=open(sys.argv[1])
+sim_f=open(sys.argv[2])
+tra2simp_f=open('./tra2simp', 'wb')
+simp2tra_f=open('./simp2tra','wb')
+
+tra2simp=defaultdict(lambda: defaultdict(int))
+simp2tra=defaultdict(lambda: defaultdict(int))
+
+
+current_tra_line=0
+num=0
+while current_tra_line!='':
+    if num%100000 ==0:
+        print (num)
+    num+=1
+    
+    current_tra_line=trad_f.readline()
+    current_simp_line=sim_f.readline()
+    if len(current_tra_line)!=len(current_simp_line):
+        print (current_tra_line)
+        print (current_simp_line)
+        continue
+    else:
+        for char_i in range(len(current_simp_line)):
+
+            tra2simp[current_tra_line[char_i]][current_simp_line[char_i]]+=1
+            simp2tra[current_simp_line[char_i]][current_tra_line[char_i]]+=1
+
+# pickle.dump(dict(tra2simp), tra2simp_f)
+# pickle.dump(dict(simp2tra), simp2tra_f)
+#with open(dict(tra2simp), 'w') as outfile:
+json.dump(dict(tra2simp), tra2simp_f)
+json.dump(dict(simp2tra), simp2tra_f)
+tra2simp_f.close()
+simp2tra_f.close()
+trad_f.close()
+sim_f.close()
