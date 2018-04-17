d2d759281590776cbde67e8cfc03743c9ff32af0
ymfa <fangyimai@gmail.com>
2018-04-17 01:06:26 +0100

    filter only numbers and letters

--- make_data.py
+++ make_data.py
@@ -2,7 +2,7 @@
 import sys, re
 from collections import Counter
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
   print("Usage: ./make_data.py FileNamePrefix [MinFreq]")
   print("I will look for input files under corpora/, output files under data/.")
   print("i.e. input files are corpora/FileNamePrefix.sc and corpora/FileNamePrefix.tc.")
@@ -10,36 +10,46 @@ if len(sys.argv) < 3:
 prefix = sys.argv[1]
 in_prefix = 'corpora/' + prefix
 out_prefix = 'data/' + prefix
+section_size = 1000000
 
 if len(sys.argv) > 2:
   min_freq = int(sys.argv[2])
 else:
   min_freq = 0  # unlimited vocab
 
-# Character ranges according to https://en.wikipedia.org/wiki/CJK_Unified_Ideographs
-# Some codepoints in these ranges are not currently in use
-non_hz = re.compile(r'[^\u3400-\u4DBF\u4E00-\u9FFF\U00020000-\U0002A6DF\U0002A700-\U0002B73F\U0002B740-\U0002B81F\U0002B820-\U0002CEAF\U0002CEB0-\U0002EBEF\uF900-\uFAFF\U0002F800-\U0002FA1F]+')
+whitespce = re.compile(r'\s+')
+numeric = re.compile(r'(\d+[\d．.－\-]*\d+|\d)')
+basic_latin = re.compile(r'([{l}]+[{l}－\-]*[{l}]+|[{l}])'.format(l='A-Za-zＡ-Ｚａ-ｚ'))
 
-def filter_hz(s, vocab):
+def filter_char(s):
+  s = whitespce.sub('', s)
+  s = numeric.sub('0', s)
+  s = basic_latin.sub('A', s)
+  return s
+
+def filter_vocab(s, vocab):
   return ' '.join(c if c in vocab else '<unk>' for c in s)
 
 def process_file(fin, fout):
   # collect sentences and build vocab
   sentences = []
   if min_freq: vocab = Counter()
-  for sentence in fin:
-    sentence = non_hz.sub('', sentence)
+  for i, sentence in enumerate(fin):
+    if i % section_size == 0: print('.', end='', flush=True)
+    sentence = filter_char(sentence)
     if sentence:
       if min_freq: vocab.update(sentence)
       sentences.append(sentence)
   if min_freq:
     vocab = set(k for k, v in vocab.items() if v >= min_freq)
-    print('vocab size:', len(vocab))
+    print('vocab size:', len(vocab), end='')
   # write file
-  for sentence in sentences:
-    if min_freq: sentence = filter_hz(sentence, vocab)
+  for i, sentence in enumerate(sentences):
+    if i % section_size == 0: print('.', end='', flush=True)
+    if min_freq: sentence = filter_vocab(sentence, vocab)
     else: sentence = ' '.join(sentence)
     fout.write(sentence + '\n')
+  print()
   return [len(s) for s in sentences]
 
 with open(in_prefix + '.tc', 'r') as fin:
@@ -57,5 +67,7 @@ if tc_counts != sc_counts:
   sys.exit()
 
 with open(out_prefix + '.de-en', 'w') as fout:
-  for tc_len in tc_counts:
+  for i, tc_len in enumerate(tc_counts):
+    if i % section_size == 0: print('.', end='', flush=True)
     fout.write(' '.join('%d %d' % (x, x) for x in range(tc_len)) + '\n')
+  print()
