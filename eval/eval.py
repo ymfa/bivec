7a77229bcdb2128c7fe8a25e434d78dcca56f047
root <hey_flora@126.com>
2018-04-18 17:31:54 +0000

    fix a bug in per char cases

--- eval/eval.py
+++ eval/eval.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[57]:
+# In[1]:
 
 
 import sys
@@ -11,9 +11,9 @@ from collections import defaultdict
 import logging
 
 #logging.basicConfig(filename='eval.log',level=logging.DEBUG)
-
+simp2trad_official='../raw_data_process_cna_cmn/simp2multitrad_official.txt'
 if len(sys.argv)<3:
-#if len(sys.argv)<5:
+# if len(sys.argv)<5:
     result='./opencc/sinica_test_opencc_s2tw'
     gold='./test_cases/sinica_gold.csv'
     logging.warning('result and gold not specified. Default is {0}, gold is {1}'.format(result,gold))
@@ -25,41 +25,41 @@ else:
 
 
 
-# In[58]:
+# In[2]:
 
 
 gold_csv = pd.read_csv(gold)
 gold_dict=gold_csv.to_dict('records')
 
 
-# In[56]:
+# In[3]:
 
 
-gold_csv['gold']
+multi_trad_official=[]
+with open(simp2trad_official) as f:
+    for line in f:
+        line=line.strip()
+        line=line.split('\t')
+        simp_char=line[0]
+        trad_chars=list(line[2].replace('～',line[0]))
+        multi_trad_official+=trad_chars
 
 
-# In[59]:
+# In[4]:
 
 
-multi_trad_official=set(list(gold_csv['gold_char']))
-multi_trad_official
+yitizi={'裡':'裏','裏':'裡','衚':'胡', '胡':'衚'}
 
 
-# In[68]:
-
-
-yitizi={'裡':'裏','裏':'裡'}#,'衚':'胡', '胡':'衚'}
-
-
-# In[70]:
+# In[5]:
 
 
 
 trad2simp={}
 line_num=0
 num_incor=0
-accu_per_char=defaultdict(float)
-num_per_char=defaultdict(float)
+error_per_char=defaultdict(int)
+num_per_char=defaultdict(int)
 
 with open(result+'_error', 'w') as error_f:
   with open(result+'_score','w') as score_f:
@@ -86,7 +86,8 @@ with open(result+'_error', 'w') as error_f:
                 if char_res in yitizi:
                     
                     if yitizi[char_res]==char_gold:
-                        accu_per_char[char_gold]=accu_per_char[char_gold]
+                        
+                        error_per_char[char_gold]=error_per_char[char_gold]
                 
                 else:
                     if char_res not in multi_trad_official:
@@ -95,21 +96,21 @@ with open(result+'_error', 'w') as error_f:
                     gold_dict[line_num]['res']=line
                     gold_dict[line_num]['line_num_in_gold']=line_num
                     writer.writerow(gold_dict[line_num])
-                    #print (char_gold,char_res,char_i,line_num,gold_csv['gold'][line_num])
                     num_incor+=1
-                    accu_per_char[char_gold]+=1
+                    error_per_char[char_gold]+=1
             else:
-                accu_per_char[char_gold]=accu_per_char[char_gold]
+                error_per_char[char_gold]=error_per_char[char_gold]
             line_num+=1 
             
 with open(result+'_score','w') as score_f:
-    fieldnames_score=['char_gold',"char_orig",'error_rate']
+    fieldnames_score=['char_gold',"char_orig",'error_num','total']
     writer_score=csv.DictWriter(score_f,fieldnames=fieldnames_score)
     writer_score.writeheader()
-    for key in accu_per_char:
-        writer_score.writerow ({'char_gold':key, 'char_orig': trad2simp[key],'error_rate':float(accu_per_char[key])/100})
-    writer_score.writerow ({'char_gold':'average', 'char_orig': 'average','error_rate':float(num_incor/len(gold_csv))})
+    for key in error_per_char:
+        writer_score.writerow ({'char_gold':key, 'char_orig': trad2simp[key],'error_num':int(error_per_char[key]), 'total':num_per_char[key]})
+    writer_score.writerow ({'char_gold':'total', 'char_orig': 'total','error_num':num_incor, 'total':len(gold_dict)})
+
 print (num_incor, 'incorrect cases')
 print('of total {0}'.format(len(gold_csv)))
-print ('error rate {0}'.format(float(num_incor/len(gold_csv))))
+print ('error rate {0}'.format(float(num_incor/len(gold_dict))))
 
